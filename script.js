fa74b15e7715dd16a1ca343edbe6404f80691e1c
Ann-Marie Greif <annx9854@stud.kea.dk>
2019-05-17 21:23:13 +0200

    kommet igang med forside, header og footer

--- script.js
+++ script.js
@@ -2,15 +2,27 @@
 window.onscroll = function () {
     scrollFunction()
 };
+document.querySelector("#mobile-button").addEventListener("click", toggleMenu);
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.querySelector("#navbar").style.padding = "20px 10px";
-        document.querySelector("#logo").style.fontSize = "25px";
+        document.querySelector("#navbar").style.padding = "5px 10px";
+        document.querySelector("#logo img").style.width = "60%";
         document.querySelector("#navbar").style.backgroundColor = "rgba(0, 0, 0, 0.58)";
     } else {
         document.querySelector("#navbar").style.padding = "30px 10px";
-        document.querySelector("#logo").style.fontSize = "35px";
+        document.querySelector("#logo img").style.width = "75%";
         document.querySelector("#navbar").style.backgroundColor = "";
+    }
+}
+
+function toggleMenu() {
+    document.querySelector("#mobile-nav").classList.toggle("hidden");
+
+    let hideNav = document.querySelector("#mobile-nav").classList.contains("hidden");
+    if (hideNav == true) {
+        document.querySelector("#mobile-button").innerHTML = "&#9776;";
+    } else {
+        document.querySelector("#mobile-button").innerHTML = "&times;";
     }
 }
