8e06b9fc14087d6676ceba65ef80d94f720aabd9
Ann-Marie Greif <annx9854@stud.kea.dk>
2019-05-24 17:57:21 +0200

    Lidt så ændringer

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.querySelector("#mobile-button").addEventListener("click", toggleMenu);
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.querySelector("#navbar").style.padding = "2px 10px";
+        document.querySelector("#navbar").style.padding = "3px 10px";
         document.querySelector("#logo img").style.width = "70%";
         document.querySelector("#navbar").style.backgroundColor = "rgba(0, 0, 0, 0.58)";
         document.querySelector("#mobile-button").style.top = "1vw";
