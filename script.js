919374ac76633ea6f692cc3970704659682ee323
Ann-Marie Greif <annx9854@stud.kea.dk>
2019-06-05 14:26:13 +0200

    kodeforklaringer

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// Når brugeren scroller ned 80px fra toppen af siden, skal størrelsen på navigation og logo blive mindre
 window.onscroll = function () {
     scrollFunction()
 };
 
 
-document.querySelector("#mobile-button").addEventListener("click", toggleMenu);
-
+// Når brugeren scroller ned 80px fra toppen af siden, skal størrelsen på navigation og logo blive mindre. Desuden så skal der komme en baggrund på navigationen.
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.querySelector("#navbar").style.padding = "3px 10px";
@@ -14,17 +13,21 @@ function scrollFunction() {
         document.querySelector("#mobile-button").style.top = "1vw";
         document.querySelector("#mobile-nav").style.top = "3.4rem";
     } else {
+        //Hvis man scroller til toppen igen, skal logo og navigation blive store igen, og baggrunden fjernes.
         document.querySelector("#navbar").style.padding = "20px 10px";
         document.querySelector("#logo img").style.width = "100%";
         document.querySelector("#navbar").style.backgroundColor = "";
         document.querySelector("#mobile-button").style.top = "6vw";
         document.querySelector("#mobile-nav").style.top = "5rem";
     }
+
+
 }
+
+document.querySelector("#mobile-button").addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     document.querySelector("#mobile-nav").classList.toggle("hidden");
-
     let hideNav = document.querySelector("#mobile-nav").classList.contains("hidden");
     if (hideNav == true) {
         document.querySelector("#mobile-button").innerHTML = "&#9776;";
